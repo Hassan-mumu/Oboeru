67364a524e507398423be39b8dbe8a48ffcb5885
Hassan-mumu <alh.diallo@students.ephec.be>
2026-04-12 22:06:26 +0200

    feat: ajout gestion des variables d'environnement

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello depuis FastAPI Oboeru!"}
+    mon_secret = os.getenv("SECRET_TEST")
+    return {"message": "Hello depuis FastAPI Oboeru!", "secret": mon_secret}
